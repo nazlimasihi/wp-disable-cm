cee9efdc3aa0f654edc02ddf82e5628094224776
nazlimasihi <74253456+nazlimasihi@users.noreply.github.com>
2020-11-11 08:40:08 +0330

    Add files via upload

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function( grunt ) {
+
+	'use strict';
+	var banner = '/**\n * <%= pkg.homepage %>\n * Copyright (c) <%= grunt.template.today("yyyy") %>\n * This file is generated automatically. Do not edit.\n */\n';
+
+	require('load-grunt-tasks')(grunt);
+
+	// Project configuration
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		addtextdomain: {
+			options: {
+				textdomain: 'disable-comments',
+			},
+			target: {
+				files: {
+					src: [ '*.php', '**/*.php', '!node_modules/**', '!php-tests/**', '!bin/**' ]
+				}
+			}
+		},
+
+		wp_readme_to_markdown: {
+			your_target: {
+				files: {
+					'README.md': 'readme.txt'
+				}
+			},
+		},
+
+		makepot: {
+			target: {
+				options: {
+					domainPath: '/languages',
+					mainFile: 'disable-comments.php',
+					potFilename: 'disable-comments.pot',
+					potHeaders: {
+						poedit: true,
+						'x-poedit-keywordslist': true
+					},
+					type: 'wp-plugin',
+					updateTimestamp: true
+				}
+			}
+		},
+
+		babel: {
+			options: {
+				sourceMap: true,
+				presets: ['@babel/preset-env']
+			},
+			dist: {
+				files: {
+					'assets/disable-comments.js': 'src/disable-comments.js'
+				}
+			}
+		}
+	} );
+
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+	grunt.registerTask( 'i18n', ['addtextdomain', 'makepot'] );
+	grunt.registerTask( 'readme', ['wp_readme_to_markdown'] );
+	grunt.registerTask( 'default', ['babel'] );
+
+	grunt.util.linefeed = '\n';
+
+};
---
+++ tests/test-plugin.php
@@ -0,0 +1,179 @@
+<?php
+/**
+ * Unit tests of this plugin
+ *
+ * @package Disable_Comments
+ */
+
+class RemoveEveryWhereTestCase extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		update_option(
+			'disable_comments_options',
+			array(
+				'db_version'          => Disable_Comments::DB_VERSION,
+				'remove_everywhere'   => true,
+				'disabled_post_types' => array( 'post', 'page', 'attachment' ),
+			)
+		);
+		$this->plugin_instance = new Disable_Comments();
+	}
+
+	function tearDown() {
+		\Brain\Monkey\tearDown();
+		parent::tearDown();
+	}
+
+	function test_init_hooks_added() {
+		$this->assertEquals( 10, has_action( 'widgets_init', array( $this->plugin_instance, 'disable_rc_widget' ) ) );
+		$this->assertEquals( 10, has_action( 'wp_headers', array( $this->plugin_instance, 'filter_wp_headers' ) ) );
+		$this->assertEquals( 9, has_action( 'template_redirect', array( $this->plugin_instance, 'filter_query' ) ) );
+		$this->assertEquals( 10, has_action( 'template_redirect', array( $this->plugin_instance, 'filter_admin_bar' ) ) );
+		$this->assertEquals( 10, has_action( 'admin_init', array( $this->plugin_instance, 'filter_admin_bar' ) ) );
+		$this->assertEquals( 10, has_action( 'plugins_loaded', array( $this->plugin_instance, 'register_text_domain' ) ) );
+		$this->assertEquals( 10, has_action( 'wp_loaded', array( $this->plugin_instance, 'init_wploaded_filters' ) ) );
+	}
+
+	function test_wp_loaded_actions() {
+		$this->plugin_instance->init_wploaded_filters();
+		$this->assertEquals( 20, has_action( 'comments_array', array( $this->plugin_instance, 'filter_existing_comments' ) ) );
+		$this->assertEquals( 20, has_action( 'comments_open', array( $this->plugin_instance, 'filter_comment_status' ) ) );
+		$this->assertEquals( 20, has_action( 'pings_open', array( $this->plugin_instance, 'filter_comment_status' ) ) );
+		$this->assertEquals( 20, has_action( 'get_comments_number', array( $this->plugin_instance, 'filter_comments_number' ) ) );
+
+		// Check that comment suport has been removed from all the post types.
+		$this->assertFalse( post_type_supports( 'post', 'comments' ) );
+		$this->assertFalse( post_type_supports( 'page', 'comments' ) );
+		$this->assertFalse( post_type_supports( 'attachment', 'comments' ) );
+	}
+
+	function test_wp_loaded_admin_actions() {
+		\Brain\Monkey\Functions\when( 'is_admin' )->justReturn( true );
+		$this->plugin_instance->init_wploaded_filters();
+
+		$this->assertEquals( 10, has_action( 'admin_print_styles-index.php', array( $this->plugin_instance, 'admin_css' ) ) );
+		$this->assertEquals( 10, has_action( 'admin_print_styles-profile.php', array( $this->plugin_instance, 'admin_css' ) ) );
+	}
+
+	function test_widget_init_actions() {
+		$this->plugin_instance->disable_rc_widget();
+		$this->assertEquals( 10, has_filter( 'show_recent_comments_widget_style', '__return_false' ) );
+	}
+
+	function test_comment_template_filter() {
+		\Brain\Monkey\Functions\when( 'is_singular' )->justReturn( true );
+		\Brain\Monkey\Functions\when( 'wp_deregister_script' )->justReturn( true );
+		$this->plugin_instance->check_comment_template();
+		// Check that this action was removed.
+		$this->assertFalse( has_action( 'wp_head', 'feed_links_extra' ) );
+	}
+
+	function test_filter_headers() {
+		$input  = array( 'X-Pingback' => 'http://example.com' );
+		$output = $this->plugin_instance->filter_wp_headers( $input );
+		$this->assertEmpty( $output );
+	}
+
+	function test_comment_feed_403() {
+		\Brain\Monkey\Functions\when( 'is_comment_feed' )->justReturn( true );
+		\Brain\Monkey\Functions\expect( 'wp_die' )->once();
+		$this->plugin_instance->filter_query();
+	}
+
+	function test_admin_bar_filter() {
+		\Brain\Monkey\Functions\when( 'is_admin_bar_showing' )->justReturn( true );
+		add_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+		$this->plugin_instance->filter_admin_bar();
+		$this->assertFalse( has_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu' ) );
+	}
+
+	function test_filter_existing_comments() {
+		$post_id = $this->factory->post->create();
+		$output  = $this->plugin_instance->filter_existing_comments( array( 'comment1', 'comment2', 'comment3' ), $post_id );
+		$this->assertEmpty( $output );
+	}
+
+	function test_filter_comment_status() {
+		$post_id = $this->factory->post->create();
+		$output  = $this->plugin_instance->filter_comment_status( 'open', $post_id );
+		$this->assertFalse( $output );
+	}
+
+	function test_filter_comments_number() {
+		$post_id = $this->factory->post->create();
+		$output  = $this->plugin_instance->filter_comments_number( 5, $post_id );
+		$this->assertEquals( 0, $output );
+	}
+}
+
+
+class RemoveIndividualTestCase extends WP_UnitTestCase {
+
+	function setUp() {
+		parent::setUp();
+		$this->reset_post_types();
+		update_option(
+			'disable_comments_options',
+			array(
+				'db_version'          => Disable_Comments::DB_VERSION,
+				'remove_everywhere'   => false,
+				'disabled_post_types' => array( 'post' ),
+			)
+		);
+		$this->plugin_instance = new Disable_Comments();
+	}
+
+	function test_init_hooks_added() {
+		$this->assertEquals( 10, has_action( 'plugins_loaded', array( $this->plugin_instance, 'register_text_domain' ) ) );
+		$this->assertEquals( 10, has_action( 'wp_loaded', array( $this->plugin_instance, 'init_wploaded_filters' ) ) );
+	}
+
+	function test_wp_loaded_actions() {
+		$this->plugin_instance->init_wploaded_filters();
+		$this->assertEquals( 20, has_action( 'comments_array', array( $this->plugin_instance, 'filter_existing_comments' ) ) );
+		$this->assertEquals( 20, has_action( 'comments_open', array( $this->plugin_instance, 'filter_comment_status' ) ) );
+		$this->assertEquals( 20, has_action( 'pings_open', array( $this->plugin_instance, 'filter_comment_status' ) ) );
+		$this->assertEquals( 20, has_action( 'get_comments_number', array( $this->plugin_instance, 'filter_comments_number' ) ) );
+
+		// Check that comment suport has been removed only from posts.
+		$this->assertFalse( post_type_supports( 'post', 'comments' ) );
+		$this->assertTrue( post_type_supports( 'page', 'comments' ) );
+	}
+
+	function test_filter_existing_comments_on_disabled_type() {
+		$post_id = $this->factory->post->create();
+		$output  = $this->plugin_instance->filter_existing_comments( array( 'comment1', 'comment2', 'comment3' ), $post_id );
+		$this->assertEmpty( $output );
+	}
+
+	function test_filter_existing_comments_on_enabled_type() {
+		$post_id = $this->factory->post->create( array( 'post_type' => 'page' ) );
+		$output  = $this->plugin_instance->filter_existing_comments( array( 'comment1', 'comment2', 'comment3' ), $post_id );
+		$this->assertEquals( array( 'comment1', 'comment2', 'comment3' ), $output );
+	}
+
+	function test_filter_comment_status_on_disabled_type() {
+		$post_id = $this->factory->post->create();
+		$output  = $this->plugin_instance->filter_comment_status( 'open', $post_id );
+		$this->assertFalse( $output );
+	}
+
+	function test_filter_comment_status_on_enabled_type() {
+		$post_id = $this->factory->post->create( array( 'post_type' => 'page' ) );
+		$output  = $this->plugin_instance->filter_comment_status( 'open', $post_id );
+		$this->assertEquals( 'open', $output );
+	}
+
+	function test_filter_comments_number_on_disabled_type() {
+		$post_id = $this->factory->post->create();
+		$output  = $this->plugin_instance->filter_comments_number( 5, $post_id );
+		$this->assertEquals( 0, $output );
+	}
+
+	function test_filter_comments_number_on_enabled_type() {
+		$post_id = $this->factory->post->create( array( 'post_type' => 'page' ) );
+		$output  = $this->plugin_instance->filter_comments_number( 5, $post_id );
+		$this->assertEquals( 5, $output );
+	}
+}
